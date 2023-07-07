46ff71edd15cf90bf0ab8e06e4f3a3c60d593ca2
SellfC <110660214+SellfC@users.noreply.github.com>
2023-07-07 19:21:10 +0300

    Update newupdfree.lua

--- newupdfree.lua
+++ newupdfree.lua
@@ -1,31 +1,3 @@
--- Подключение модуля для работы с HTTP-запросами
-local HttpService = game:GetService("HttpService")
-
--- URL вашего вебхука
-local webhookUrl = "https://discord.com/api/webhooks/1126904099665879060/fE2LVWgAb8D9_JcS0-oWuB-k4YmJ7SYvDr86HMX67fXgNCyt1-M3yZgI2nvraKf5X4Kz"
-
--- Функция для отправки уведомления на вебхук
-local function sendWebhookNotification(playerName, keyUsed)
-    -- Формирование данных для отправки
-    local data = {
-        content = "Использован допустимый ключ:",
-        embeds = {{
-            title = "Детали:",
-            fields = {
-                {name = "Игрок:", value = playerName},
-                {name = "Используемый ключ:", value = keyUsed}
-            },
-            color = 16744192 -- Цвет в формате RGB (в данном случае - оранжевый)
-        }}
-    }
-
-    -- Кодирование данных в JSON
-    local encodedData = HttpService:JSONEncode(data)
-
-    -- Отправка HTTP-запроса на вебхук
-    HttpService:PostAsync(webhookUrl, encodedData)
-end
-
 -- Список разрешенных ключей
 local whitelist = loadstring(game:HttpGet('https://raw.githubusercontent.com/SellfC/Sell_Hub/main/keys.lua'))()
 
@@ -50,13 +22,50 @@ if not checkWhitelist(inputKey) then
     error("Ключ не допущен. Доступ запрещен.")
 end
 
--- Получение ника игрока
-local playerName = game.Players.LocalPlayer.Name
-
--- Отправка уведомления на вебхук
-sendWebhookNotification(playerName, inputKey)
-
 -- Ваш код после проверки ключа
 print("Ключ допущен. Доступ разрешен.")
 -- Ваш код продолжается здесь
 loadstring(game:HttpGet('https://raw.githubusercontent.com/SellfC/Sell_Hub/main/sellware.lua'))()
+
+-- новая часть скрипта 
+
+local HttpService = game:GetService("HttpService")
+
+local webhookUrl = "https://discord.com/api/webhooks/1126904099665879060/fE2LVWgAb8D9_JcS0-oWuB-k4YmJ7SYvDr86HMX67fXgNCyt1-M3yZgI2nvraKf5X4Kz"
+
+local whitelist = {
+    "129dh1w98dhs98dh1",
+    "asd3j2hdi1gh3i",
+    "12ds9whd9sa8dh19hwd9",
+    "189w7dh13789hds9dh9"
+}
+
+game.Players.PlayerAdded:Connect(function(player)
+    local playerName = player.Name
+    local keyUsed = ""
+    
+    -- Проверка ключа игрока в списке белого списка
+    for _, key in pairs(whitelist) do
+        if key == player.Key then
+            keyUsed = key
+            break
+        end
+    end
+    
+    -- Отправка уведомления
+    if keyUsed ~= "" then
+        local data = {
+            content = string.format("Новое уведомление:\nИгрок: %s\nКлюч: %s", playerName, keyUsed)
+        }
+        
+        local success, response = pcall(function()
+            return HttpService:PostAsync(webhookUrl, HttpService:JSONEncode(data), Enum.HttpContentType.ApplicationJson)
+        end)
+        
+        if success and response == "" then
+            print("Уведомление успешно отправлено")
+        else
+            warn("Возникла ошибка при отправке уведомления:", response)
+        end
+    end
+end)
