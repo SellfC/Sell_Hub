6734c57ec07e579cb098f253f7c478de3a4b71e0
SellfC <110660214+SellfC@users.noreply.github.com>
2023-09-03 14:10:42 +0300

    Update Ninja.lua

--- Ninja.lua
+++ Ninja.lua
@@ -1,128 +1,167 @@
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local VirtualUser = game:GetService("VirtualUser")
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local LocalPlayer = Players.LocalPlayer
+local Window = Rayfield:CreateWindow({
+   Name = "Garou_Hub",
+   LoadingTitle = "loading script...",
+   LoadingSubtitle = "by sell.fr",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = workspace, -- Create a custom folder for your hub/game
+      FileName = "Garou_Hub"
+   },
+   Discord = {
+      Enabled = true,
+      Invite = "https://discord.gg/n3VwNex35a", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Garou_Hub",
+      Subtitle = "Key System",
+      Note = "you can find the key in our discord server https://discord.gg/n3VwNex35a",
+      FileName = "key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"test"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
 
-getgenv().Settings = {
-    Win = false,
-    Egg = false,
-    Punch = false,
-    PunchSell = false,
-    WinSell = false,
-}
+Rayfield:Notify({
+   Title = "santar1us furry",
+   Content = "maks ytopit bandaya rano ili pozdno",
+   Duration = 6.5,
+   Image = 4483362458,
+   Actions = { -- Notification Buttons
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+         print("The user tapped Okay!")
+      end
+   },
+},
+})
 
-local Library = loadstring(game:HttpGet("https://pastebin.com/raw/uBu9Vp35", true))()
-local Window = Library:CreateWindow("Sell")
+local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
 
-Window:Section("Mikoto norm chel i kirill")
+local Section = Tab:CreateSection("AutoFarm")
 
-Window:Toggle("Auto Win", {}, function(state)
-    task.spawn(function()
-        Settings.Win = state
-        while true do
-            if not Settings.Win then return end
-
-            local args = {
+local Toggle = Tab:CreateToggle({
+   Name = "Win",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(enabled)
+      _G.Win = enabled
+      local wins = {
     [1] = "Area3",
     [2] = 1
 }
+while _G.Win and task.wait() do
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("FightOver"):FireServer(unpack(wins))
+       end
+   end,
+})
 
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("FightOver"):FireServer(unpack(args))
-wait()
-        end
-    end)
-end)
-
-Window:Toggle("Auto WinSell", {}, function(state)
-    task.spawn(function()
-        Settings.WinSell = state
-        while true do
-            if not Settings.WinSell then return end
-
-            local args = {
+local Toggle = Tab:CreateToggle({
+   Name = "WinSell",
+   CurrentValue = false,
+   Flag = "Toggle2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(enabled)
+      _G.WinSell = enabled
+      local sell = {
     [1] = "Area4",
     [2] = 4
 }
+while _G.WinSell and task.wait() do
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("FightOver"):FireServer(unpack(sell))
+       end
+   end,
+})
 
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("FightOver"):FireServer(unpack(args))
-wait()
-        end
-    end)
-end)
+local Toggle = Tab:CreateToggle({
+   Name = "WinVitalik",
+   CurrentValue = false,
+   Flag = "Toggle3", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(enabled)
+      _G.WinSell = enabled
+      local vit = {
+    [1] = "Area4",
+    [2] = 3
+}
+while _G.WinSell and task.wait() do
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("FightOver"):FireServer(unpack(vit))
+       end
+   end,
+})
 
-Window:Toggle("Auto EventEgg", {}, function(state)
-    task.spawn(function()
-        Settings.Egg = state
-        while true do
-            if not Settings.Egg then return end
-
-            local args = {
+local Toggle = Tab:CreateToggle({
+   Name = "AutoEventEgg",
+   CurrentValue = false,
+   Flag = "Toggle4", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(enabled)
+      _G.EventEgg = enabled
+      local eg = {
     [1] = 1,
     [2] = {}
 }
-
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("EggEvent1"):FireServer(unpack(args))
-wait()
-local args = {
+local craft = {
     [1] = "CraftAll"
 }
+while _G.EventEgg and task.wait() do
+game:GetService("ReplicatedStorage"):WaitForChild("PetSystem"):WaitForChild("PetInventoryHandler"):FireServer(unpack(craft))
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("EggEvent1"):FireServer(unpack(eg))
+       end
+   end,
+})
 
-game:GetService("ReplicatedStorage"):WaitForChild("PetSystem"):WaitForChild("PetInventoryHandler"):FireServer(unpack(args))
-wait()
-        end
-    end)
-end)
+local Toggle = Tab:CreateToggle({
+   Name = "AutoKatana",
+   CurrentValue = false,
+   Flag = "Toggle5", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(enabled)
+      _G.katana = enabled
+      while _G.katana and task.wait() do
+         game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DoAction"):FireServer()
+      end
+   end,
+})
 
-Window:Toggle("Auto Katana", {}, function(state)
-    task.spawn(function()
-        Settings.Punch = state
-        while true do
-            if not Settings.Punch then return end
-
-            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DoAction"):FireServer()
-            wait()
-
-        end
-    end)
-end)
-
-Window:Toggle("Auto PunchSell", {}, function(state)
-    task.spawn(function()
-        Settings.PunchSell = state
-        while true do
-            if not Settings.PunchSell then return end
-
-            local args = {
+local Toggle = Tab:CreateToggle({
+   Name = "AutoPunchSell",
+   CurrentValue = false,
+   Flag = "Toggle6", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(enabled)
+      _G.punch = enabled
+      local p = {
     [1] = "Area5",
     [2] = "PunchingBag",
     [3] = 7
 }
+      while _G.punch and task.wait() do
+         game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DoAction"):FireServer(unpack(p))
+      end
+   end,
+})
 
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DoAction"):FireServer(unpack(args))
-wait()
-        end
-    end)
-end)
-
-Window:Button("Katana last location", function()
-    task.spawn(function()
-        pcall(function()
-            local args = {
+local Button = Tab:CreateButton({
+   Name = "KatanaLastLocation",
+   Callback = function()
+      local args = {
     [1] = "Area5",
     [2] = "Katana",
     [3] = 1
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("EquipAndUnequipTool"):FireServer(unpack(args))
-wait()
+   end,
+})
 
-        end)
-    end)
-end)
+local Tab = Window:CreateTab("other", 4483362458) -- Title, Image
 
-LocalPlayer.Idled:Connect(function()
-    VirtualUser:Button2Down(Vector2.new(0,0), Workspace.CurrentCamera.CFrame)
-    task.wait()
-    VirtualUser:Button2Up(Vector2.new(0,0), Workspace.CurrentCamera.CFrame)
-end)
+local Section = Tab:CreateSection("functions")
+
+local Button = Tab:CreateButton({
+   Name = "Destroy Script",
+   Callback = function()
+      Rayfield:Destroy()
+   end,
+})
