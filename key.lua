75b33a142a87bb5066d1d11cf4605321b9d43c60
SellfC <110660214+SellfC@users.noreply.github.com>
2023-11-20 22:24:32 +0300

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,7 +1,9 @@
-local keys = loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/keys.lua"))()
+local keys = {loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/keys.lua"))()}
 
-if getgenv().key == keys then
+if getgenv().key == unpack(keys) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/scripthubbysell.lua"))()
 else
     loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end
+getgenv().key = "sdf21p9i1d30d10"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/keysys.lua"))()
