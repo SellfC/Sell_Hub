d54422847bad766d0cffbfa555e6510e90f366a5
SellfC <110660214+SellfC@users.noreply.github.com>
2023-11-20 22:56:31 +0300

    Update keysys.lua

--- keysys.lua
+++ keysys.lua
@@ -1,7 +1,10 @@
 local keys = loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/keys.lua"))()
+local player = game.Players.LocalPlayer
 
 for i,v in pairs(keys) do 
     if v == getgenv().key then 
         loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/scripthubbysell.lua"))()
+    else
+        player:Kick("Not" .. "whitelisted")
     end
 end
