baf1cd854c8eb327430a8e2c54a722d2df3aff07
SellfC <110660214+SellfC@users.noreply.github.com>
2023-11-20 22:07:11 +0300

    Update keysys.lua

--- keysys.lua
+++ keysys.lua
@@ -2,6 +2,8 @@ local keys = {
     "sdf21p9i1d30d10"
 }
 
-if getgenv().key == keys then
-    print("lox")
+if getgenv().key == unpack(keys) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/scripthubbysell.lua"))()
+else
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end
