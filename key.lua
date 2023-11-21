6a2872157156c42ec5a8e8edaee4bf7480296005
SellfC <110660214+SellfC@users.noreply.github.com>
2023-11-21 18:20:29 +0300

    Delete key.lua

--- key.lua
+++
@@ -1,7 +0,0 @@
-local keys = "https://raw.githubusercontent.com/SellfC/Sell_Hub/main/keys.lua"
-
-if getgenv().key == keys then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/scripthubbysell.lua"))()
-else
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-end
