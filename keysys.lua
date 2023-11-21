66f82be06f06989946db759d4d2a901e219a113b
SellfC <110660214+SellfC@users.noreply.github.com>
2023-11-21 17:49:32 +0300

    Update keysys.lua

--- keysys.lua
+++ keysys.lua
@@ -3,7 +3,7 @@ local player = game.Players.LocalPlayer
 
 for i,v in pairs(keys) do 
     if v == getgenv().key then 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/scriptsloader.lua"))()-- your code
+        loadstring(game:HttpGet("https://pastebin.com/raw/8g1CzD3z"))()-- your code
         print("scriploader true")
     else
         setclipboard("https://discord.gg/gnDtRn7wbn")
