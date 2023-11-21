fbc56a38ce2072bc9797ff8ecc39a255ef68abe8
SellfC <110660214+SellfC@users.noreply.github.com>
2023-11-21 17:41:51 +0300

    Update keysys.lua

--- keysys.lua
+++ keysys.lua
@@ -2,8 +2,9 @@ local keys = loadstring(game:HttpGet("https://pastebin.com/raw/Va6zmbPk"))()
 local player = game.Players.LocalPlayer
 
 for i,v in pairs(keys) do 
-    if v == getgenv().key task.wait(10) then 
+    if v == getgenv().key then 
         loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/Sell_Hub/main/scriptsloader.lua"))()-- your code
+        print("scriploader true")
     else
         setclipboard("https://discord.gg/gnDtRn7wbn")
         player:Kick("Not" .. " whitelisted" .. " Key in Discord" .. " Link copied")
