a552635f8c98d607b73ac1ea988545c66ace0baf
SellfC <110660214+SellfC@users.noreply.github.com>
2024-04-02 18:09:47 +0300

    Update tiolet.lua

--- tiolet.lua
+++ tiolet.lua
@@ -1,6 +1,6 @@
 getgenv().Executed = true
 getgenv().Key = "k8f732e0f88c9d260d9ae406"
-repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(3)until Joebiden
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(1)until Joebiden
 
 while getgenv().Executed and task.wait(1000000000000) do 
    print("VITALOK SOSET XUI")
