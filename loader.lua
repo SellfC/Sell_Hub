2006348e8d1f9dfb6b21205339d11254ba4a6dcc
SellfC <110660214+SellfC@users.noreply.github.com>
2024-04-02 18:18:32 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,48 +1,29 @@
 local repo = "https://raw.githubusercontent.com/SellfC/Sell_Hub/main/"
-local function LoadScript(ScriptName)
-	pcall(function()
-		t = 0
-		repeat
-			local s, r = pcall(function()
-				loadstring(game:HttpGet(repo .. ScriptName))()
-			end)
-			if not s then
-				spawn(function()
-					error(r)
-				end)
-			end
-			t = t + 1
-			wait(60)
-		until getgenv().Executed or t >= 30
-	end)
-end
 local Id = game.PlaceId
 local GameId = game.GameId
-local player = game.Players.LocalPlayer
-local PlaceIds = {
-	["Ninjasim"] = { 13999740606 },
-	["ps"] = { 14236123211 },
-    ["BF"] = { 2753915549, 4442272183, 7449423635 },
-    ["testplace"] = { 4483381587 },
-	["ttd"] = { 13775256536, 14082129854 },
+
+local Games = {
+	["BF"] = {
+		FileName = "w-azure",
+		PlaceId = { 2753915549, 4442272183, 7449423635 },
+		GameName = "Blox Fruits",
+	},
+	["TTD"] = {
+		FileName = "tiplet",
+		PlaceId = { 13775256536, 14082129854 },
+		GameName = "toilet tower defense",
+	}
 }
 
-if table.find(PlaceIds["ps"], Id) then -- Punch sim
-        if getgenv().AndroidScript then
-        LoadScript("punchandroid.lua")
-	else
-	LoadScript("punchpc.lua")
+local function LoadScript(name)
+	local data = Games[name]
+	getgenv().GameName = data.GameName
+
+	loadstring(game:HttpGet(repo .. data.FileName .. ".lua"))()
+end
+
+for name, data in next, Games do
+	for _, v in next, data.PlaceId do
+		if Id == v or GameId == v then LoadScript(name) end
 	end
-elseif table.find(PlaceIds["Ninjasim"], Id) then -- Ninja Sim
-	LoadScript("Ninja.lua")
-	print("script load")
-elseif table.find(PlaceIds["testplace"], Id) then -- Ninja Sim
-	LoadScript("scripthubbysell.lua")
-	print("script load")
-elseif table.find(PlaceIds["BF"], Id) then -- Ninja Sim
-	LoadScript("w-azure.lua")
-	print("script load")
-elseif table.find(PlaceIds["ttd"], Id) then -- Ninja Sim
-	LoadScript("tiolet.lua")
-	print("script load")
 end
