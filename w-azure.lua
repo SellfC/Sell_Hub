8621e014620c6b3dfa8e1507486b379dd8d7a42b
SellfC <110660214+SellfC@users.noreply.github.com>
2024-04-02 18:14:23 +0300

    Update w-azure.lua

--- w-azure.lua
+++ w-azure.lua
@@ -1,11 +1,11 @@
-print("execute")
 getgenv().Executed = true
+while getgenv().Executed and task.wait(1000000000000) do 
+   print("VITALOK SOSET XUI")
+end
+
+print("execute")
 script_key = "GVnjEUyrpvmpjjywIGZWrdoRjnWSxZHP"
 getgenv().Team = "Pirates"
 getgenv().FixCrash = true -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
 getgenv().FixCrash2 = true -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
-
-while getgenv().Executed and task.wait(1000000000000) do 
-   print("VITALOK SOSET XUI")
-end
