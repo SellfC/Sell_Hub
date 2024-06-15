7624c70190d830bcb5d1366f44b4e08b4c526fef
SellfC <110660214+SellfC@users.noreply.github.com>
2024-06-15 12:29:41 +0300

    Create maruad.lua

---
+++ maruad.lua
@@ -0,0 +1,4 @@
+getgenv().Key = "MARU-J6CB3-79FB-RPL94-W1E7-ZLF6T"
+getgenv().id = "952168231827812412"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
